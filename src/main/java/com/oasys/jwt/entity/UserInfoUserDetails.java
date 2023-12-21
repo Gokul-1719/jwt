b6820de45360b1d26a7bd7842f0b095bba15ac28
Gokul-1719 <gokulgokul3237@gmail.com>
2023-12-21 17:30:28 +0530

    jwt

---
+++ src/main/java/com/oasys/jwt/entity/UserInfoUserDetails.java
@@ -0,0 +1,67 @@
+package com.oasys.jwt.entity;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.oasys.jwt.entity.*;
+
+public class UserInfoUserDetails implements UserDetails{
+	
+	public String username;
+	public String password;
+	public List<GrantedAuthority> authorities;
+	
+	
+	public UserInfoUserDetails(UserInfo user) {
+		username=user.getName();
+		password=user.getPassword();
+		authorities=Arrays.stream(user.getRoles().split(","))
+				.map(i->new SimpleGrantedAuthority(i)).collect(Collectors.toList());
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.password;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.username;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
